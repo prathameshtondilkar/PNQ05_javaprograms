30bfc7ec45415a8cf41dcb6ed8a02648201102fe
hp <hp@LAPTOP-CBUD3FCD>
2024-07-11 09:08:11 +0530

    Hello World program

---
+++ DAY_01/src/com/day_01/demo/myApp.java
@@ -0,0 +1,14 @@
+package com.day_01.demo;
+
+
+
+public class myApp {
+
+	public static void main(String[] args)
+	{
+		System.out.println("Hello World");
+		System.out.print("Bye World \n");
+		
+		
+	}
+}
